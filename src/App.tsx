d52a467ba4caa11ff3f6adf3050718013244f22c
Kyle Copeland <copelandky@gmail.com>
2020-04-11 11:31:21 -0700

    feat: add accessible icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 function App() {
+  const [value, setValue] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div role='search'>
+          <input aria-label='Search for comics' value={value} onChange={(e) => setValue(e.target.value)} />
+          {value !== '' && (
+          <button aria-label='Clear the search field'  onClick={() => setValue('')}>
+            <i className='fa fa-times' aria-hidden='true'/>
+          </button>
+          )}
+       </div>
       </header>
     </div>
   );
